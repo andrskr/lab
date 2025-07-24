97f73db742301c82a4a834aefdc98c529fe8d5a4
Andrii Skrebets <andriiskrebets@gmail.com>
2025-07-24 21:04:59 +0100

    nonce test

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,10 +8,31 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+function getRandomValue() {
+  const array = new Uint8Array(16);
+  crypto.getRandomValues(array);
+
+  return array;
+}
+
+function toHexString(byteArray: Uint8Array) {
+  return Array.from(byteArray, (byte) =>
+    byte.toString(16).padStart(2, "0")
+  ).join("");
+}
+
+function generateNonce() {
+  const randomValue = getRandomValue();
+
+  return toHexString(randomValue);
+}
+
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+  const nonce = generateNonce();
+
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE, nonce };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Welcome message={loaderData.message} nonce={loaderData.nonce} />;
 }
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,13 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
-export function Welcome({ message }: { message: string }) {
+export function Welcome({
+  message,
+  nonce,
+}: {
+  message: string;
+  nonce?: string;
+}) {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -19,6 +25,10 @@ export function Welcome({ message }: { message: string }) {
             />
           </div>
         </header>
+        <h1>
+          Nonce:{" "}
+          <span className="text-blue-600 dark:text-blue-400">{nonce}</span>
+        </h1>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
