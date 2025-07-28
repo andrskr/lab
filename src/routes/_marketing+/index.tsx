dec61266bf8f415713584c5d9eb5b26bb9d150dd
Andrii Skrebets <andriiskrebets@gmail.com>
2025-07-28 21:17:02 +0100

    scroll

--- src/routes/_marketing+/index.tsx
+++ src/routes/_marketing+/index.tsx
@@ -12,6 +12,73 @@ import { useModernLayoutEffect } from '~/common/lib/use-modern-layout-effect';
 
 const SCROLL_DEBOUNCE_TIME_MS = 100;
 
+/**
+ * The target for navigation within the scroller.
+ *
+ * - `start` first visible item inside the scroller
+ * - `end` last visible item inside the scroller
+ * - `next` first invisible item after the `end` item (outside of viewport)
+ * - `previous` first invisible item before the `start` item (outside of viewport)
+ */
+type NavigateTarget = 'start' | 'end' | 'next' | 'previous';
+
+interface NavigateOptions {
+  /**
+   * The alignment of the item in the viewport after scrolling.
+   *
+   * - `start` aligns the item to the start of the viewport.
+   * - `center` aligns the item to the center of the viewport.
+   * - `end` aligns the item to the end of the viewport.
+   *
+   * Defaults to `start`.
+   */
+  align?: 'start' | 'center' | 'end';
+}
+
+function findTargetElement(target: NavigateTarget) {
+  const elements = document.querySelectorAll<HTMLElement>('[data-scroller-item]');
+
+  if (elements.length === 0) {
+    return null;
+  }
+
+  const elementsList = Array.from(elements);
+
+  if (target === 'end' || target === 'next') {
+    elementsList.reverse();
+  }
+
+  const targetElement = elementsList.find((element) => {
+    return element.dataset.visible === '';
+  });
+
+  if (!targetElement) {
+    return null;
+  }
+
+  switch (target) {
+    case 'start':
+    case 'end': {
+      return targetElement;
+    }
+
+    case 'next':
+    case 'previous': {
+      const index = elementsList.indexOf(targetElement);
+      const newTargetElement = elementsList.at(index - 1);
+
+      return newTargetElement ?? null;
+    }
+
+    default: {
+      return targetElement;
+    }
+  }
+}
+
+/** The minimum intersection ratio for an item to be considered visible. */
+const VIEW_TOLERANCE = 0.5;
+
 interface ItemProps extends ComponentProps<'div'> {
   scrollerElement: HTMLDivElement | null;
 }
@@ -22,20 +89,44 @@ function Item(props: ItemProps) {
     ref: observerRef,
     inView,
     entry,
-  } = useInView({ root: scrollerElement, threshold: [0, 0.5, 1] });
+  } = useInView({ root: scrollerElement, threshold: [0, VIEW_TOLERANCE, 1] });
   const mergedRef = useMergeRefs(ref, observerRef);
+  const isVisible = inView && (entry?.intersectionRatio ?? 0) > VIEW_TOLERANCE;
 
   return (
     <div
       data-scroller-item=""
-      data-intersection-ratio={entry?.intersectionRatio}
-      data-in-view={dataAttribute(inView)}
+      data-visible={dataAttribute(isVisible)}
       ref={mergedRef}
       className="grid h-full w-[300px] snap-start place-content-center border"
     >
       <div className="text-2xl font-bold">{children}</div>
     </div>
   );
+}
+
+function scrollElement(scroller: HTMLElement, target: HTMLElement, options: NavigateOptions) {
+  // const { align = 'start' } = options;
+  // const { scrollLeft, clientWidth } = scroller;
+  // const targetRect = target.getBoundingClientRect();
+  // const scrollerRect = scroller.getBoundingClientRect();
+  // const targetLeft = targetRect.left - scrollerRect.left + scrollLeft;
+  // const targetRight = targetRect.right - scrollerRect.left + scrollLeft;
+  // const targetWidth = targetRect.width;
+  // let newScrollLeft = targetLeft;
+  // if (align === 'center') {
+  //   newScrollLeft = targetLeft - (clientWidth - targetWidth) / 2;
+  // } else if (align === 'end') {
+  //   newScrollLeft = targetRight - clientWidth;
+  // }
+  // // Ensure the new scroll position is within bounds
+  // newScrollLeft = Math.max(0, newScrollLeft);
+  // newScrollLeft = Math.min(newScrollLeft, scroller.scrollWidth - clientWidth);
+  // // Scroll to the new position
+  // scroller.scrollTo({
+  //   left: newScrollLeft,
+  //   behavior: 'smooth',
+  // });
 }
 
 export default function Index() {
@@ -108,6 +199,20 @@ export default function Index() {
     [handleScrollStateSync, scrollerElement],
   );
 
+  const navigate = useEventCallback((target: NavigateTarget, options: NavigateOptions) => {
+    if (scrollerElement === null) {
+      return;
+    }
+
+    const targetElement = findTargetElement(target);
+
+    if (!targetElement) {
+      return;
+    }
+
+    scrollElement(scrollerElement, targetElement, options);
+  });
+
   return (
     <div>
       <div
@@ -128,6 +233,9 @@ export default function Index() {
 
       <div className="flex justify-center gap-4 py-2">
         <button
+          onClick={() => {
+            navigate('start', { align: 'end' });
+          }}
           disabled={!canGoToPrevious}
           type="button"
           className="rounded-md bg-blue-500 px-3 py-2 text-sm text-white disabled:opacity-50"
@@ -136,6 +244,9 @@ export default function Index() {
         </button>
 
         <button
+          onClick={() => {
+            navigate('end', { align: 'start' });
+          }}
           disabled={!canGoToNext}
           type="button"
           className="rounded-md bg-blue-500 px-3 py-2 text-sm text-white disabled:opacity-50"
