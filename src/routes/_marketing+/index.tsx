be2b45595653e1947516a1481a215e4888792170
Andrii Skrebets <andriiskrebets@gmail.com>
2025-07-29 20:18:09 +0100

    items align

--- src/routes/_marketing+/index.tsx
+++ src/routes/_marketing+/index.tsx
@@ -136,7 +136,10 @@ function Item(props: ItemProps) {
       data-scroller-item=""
       data-visible={dataAttribute(isVisible)}
       ref={mergedRef}
-      className="grid h-full w-[300px] snap-start place-content-center border"
+      className={cx(
+        '[scroll-snap-align:var(--scroller-item-align,start)]',
+        'grid h-full w-[300px] place-content-center border',
+      )}
     >
       <div className="text-2xl font-bold">{children}</div>
     </div>
@@ -265,6 +268,8 @@ export default function Index() {
         className={cx(
           '[--scroller-items-gutter:theme(spacing.4)]',
           '[--scroller-edge-gutter:theme(spacing.4)]',
+          /** TODO: control using prop (e.g. align) */
+          '[--scroller-item-align:start]',
           '[--scroller-cols:calc((100%-(var(--scroller-visible-items)-1)*var(--scroller-items-gutter))_/_var(--scroller-visible-items))]',
           'grid h-[400px] snap-always scroll-px-(--scroller-edge-gutter) auto-cols-(--scroller-cols) grid-flow-col gap-(--scroller-items-gutter) overflow-x-auto overflow-y-hidden overscroll-contain px-(--scroller-edge-gutter) not-data-animation:snap-x not-data-animation:snap-mandatory motion-safe:scroll-smooth',
         )}
